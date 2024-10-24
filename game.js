18d3d78d59a53ea282c9e517bf632fff4ebdb10d
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-10-24 08:28:31 +0000

    Add: name data pulled throught to game

--- game.js
+++ game.js
@@ -11,12 +11,16 @@ window.addEventListener('load', function () {
 // take OPTIONAL player name, close modal and display player name in 'Name' window - code adapted from Keith Paterson https://www.youtube.com/watch?v=KB6Yg5hNrqc
 
 let playerName = document.querySelector('#playerName');
-let nameEntered = document.querySelector('#nameEntered');
+let name = document.querySelector('#name');
 let enterName = document.querySelector('.button');
 
 enterName.addEventListener('click', () => {
 const modal = document.querySelector('#start-modal');
 modal.style.display = 'none';
 
-nameEntered.innerHTML = playerName.value;
+name.innerHTML = playerName.value;
 });
+
+//hit enter key to close modal 
+//document.getElementById("answer-box").addEventListener("keydown", function(event){
+    //if (event.key === "Enter")
